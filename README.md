570e1ce9b94a7ccd12ec5585653944901cb86162
Alan Buss <al@constellationwebservices.com>
2016-12-25 21:20:08 -0600

    Added observation instructions, fixed Observation construction

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ The [NOAA documentation](https://forecast-v3.weather.gov/documentation)
 Forecasts can be retrieved for a point or a station.
 Both the hourly and daily forecast return 7 days worth of data.
 
+Observations are for the last 5 hours
+
 Basic Usage for Forecast for a given point
 ==========================================
 
@@ -20,4 +22,21 @@ Basic Usage for Forecast for a given point
     $hourly = $noaa->getHourlyForecast();
     foreach($hourly as $forecast){
         echo $forecast->getStart('D, d M H:i:s') . " Temp:" . $forecast->getTemperature() . "\n";
+    }
+
+Basic Usage for Observation for a given point
+==========================================
+
+    <?php
+	include "noaa/autoload.php"; //only needed if not using composer bootstrap
+    use noaa\util\Cache;
+    use noaa\Point;
+    use noaa\NOAA;
+
+    $noaa = new NOAA(new Cache);
+    $point = new Point(43.43, -90.80);
+    $noaa->setPoint($point);
+    $observations = $noaa->getObservations();
+    foreach($observations as $observation){
+        echo $observation->getTime('D, d M H:i:s') . " Temp:" . $observation->getTemperature() . "\n";
     }
--- noaa/Observation.php
+++ noaa/Observation.php
@@ -20,6 +20,11 @@ use noaa\Station;
  */
 class Observation extends Base{
 
+		public function __construct($props = null){
+				if($props){
+						$this->setProperties($props);
+				}
+		}
 		/**
 		 * @string id for this observation
 		 */
@@ -57,78 +62,77 @@ class Observation extends Base{
 		 * @return float temperature in degrees C
 		 */
 		public function getTemperature(){
-				return $this->properties->properties->temperature;
+				return $this->properties->properties->temperature->value;
 		}
 
 		/**
 		 * @return float dewpoint in degrees C
 		 */
 		public function getDewpoint(){
-				return $this->properties->properties->dewpoint;
+				return $this->properties->properties->dewpoint->value;
 		}
 
 		/**
 		 * @return int wind direction in degrees
 		 */
 		public function getWindDirection(){
-				return $this->properties->properties->windDirection;
+				return $this->properties->properties->windDirection->value;
 		}
 
 		/**
 		 * @return float wind speed in m/s
 		 */
 		public function getWindSpeed(){
-				return $this->properties->properties->windSpeed;
+				return $this->properties->properties->windSpeed->value;
 		}
 
 		/**
 		 * @return float wind gusts in m/s
 		 */
 		public function windGust(){
-				return $this->properties->properties->windGust;
+				return $this->properties->properties->windGust->value;
 		}
 
 		/**
 		 * @return int barometric pressure in Pa
 		 */
 		public function getBarometricPressure(){
-				return $this->properties->properties->barometricPressure;
+				return $this->properties->properties->barometricPressure->value;
 		}
 
 		/**
 		 * @return int visibility in meters
 		 */
 		public function getVisibility(){
-				return $this->properties->properties->visibility;
+				return $this->properties->properties->visibility->value;
 		}
 
 		/**
 		 * @return float last hours precip in meters
 		 */
 		public function getPrecipitationLastHour(){
-				return $this->properties->properties->precipitationLastHour;
+				return $this->properties->properties->precipitationLastHour->value;
 		}
 
 		/**
 		 * @return float relative humidity in percent
 		 */
 		public function getRelativeHumidity(){
-				return $this->properties->properties->relativeHumidity;
+				return $this->properties->properties->relativeHumidity->value;
 		}
 
 		/**
 		 * @return float wind chill in degrees C
 		 */
 		public function getWindChill(){
-				return $this->properties->properties->windChill;
+				return $this->properties->properties->windChill->value;
 		}
 
 		/**
 		 * @return float heat index in degrees C
 		 */
 		public function getHeatIndex(){
-				return $this->properties->properties->heatIndex;
+				return $this->properties->properties->heatIndex->value;
 		}
 }
-
 
